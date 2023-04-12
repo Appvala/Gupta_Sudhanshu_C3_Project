cf558590262d8b9bda85a597b700e72d8e66684c
appvala <sudhanshusaurabhgupta@gmail.com>
2023-04-12 13:28:42 +0530

    Part 3: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -65,5 +65,17 @@ public class Restaurant {
     public String getName() {
         return name;
     }
-
+    public int totalValue(List<String> itemNames) {
+        int totalPrice = 0;
+        if (itemNames != null) {
+            for (String name : itemNames) {
+                for (Item item : menu) {
+                    if (item.getName().equals(name))
+                        totalPrice = totalPrice + item.getPrice();
+                }
+                System.out.println(totalPrice);
+            }
+        }
+        return totalPrice;
+    }
 }
